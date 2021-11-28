427f8139c476e7c06903ad220b690b4dbaff4583
Nicholas Yoon <nyoon8360@gmail.com>
2021-11-28 16:14:21 -0500

    made recently procured bar component sticky to app component.

--- NicholasYoonProjectCaravan/src/modules/my/recentlyProcuredBar/recentlyProcuredBar.js
+++ NicholasYoonProjectCaravan/src/modules/my/recentlyProcuredBar/recentlyProcuredBar.js
@@ -5,37 +5,37 @@ export default class RecentlyProcuredBar extends LightningElement {
         {
             ProductID : "1",
             ProductName : "Rejuvinating Potion",
-            ProductPicURL : "./resources/images/potion1.png",
-            CurrentBid: "$14"
+            ProductPicURL : "./resources/images/newAuctionItems/potion1.png",
+            CurrentBid: "$14",
+            AuctionActive: true
         },
         {
             ProductID : "2",
             ProductName : "Unidentified Grimoire",
-            ProductPicURL : "./resources/images/grimoire.png",
-            CurrentBid: "$2,204"
+            ProductPicURL : "./resources/images/newAuctionItems/grimoire.png",
+            CurrentBid: "$2,204",
+            AuctionActive: true
         },
         {
             ProductID : "3",
             ProductName : "Enchanted Halberd",
-            ProductPicURL : "./resources/images/halberd.png",
-            CurrentBid: "$1,436"
+            ProductPicURL : "./resources/images/newAuctionItems/halberd.png",
+            CurrentBid: "$1,436",
+            AuctionActive: true
         },
         {
             ProductID : "4",
             ProductName : "Tier 3 Spell Scroll",
-            ProductPicURL : "./resources/images/scroll1.png",
-            CurrentBid: "$89"
+            ProductPicURL : "./resources/images/newAuctionItems/scroll1.png",
+            CurrentBid: "$89",
+            AuctionActive: true
         },
         {
             ProductID : "5",
             ProductName : "Damascus Longsword",
-            ProductPicURL : "./resources/images/sword1.png",
-            CurrentBid: "$2,967"
+            ProductPicURL : "./resources/images/newAuctionItems/sword1.png",
+            CurrentBid: "$2,967",
+            AuctionActive: true
         },
     ]
-    
-    constructor() {
-        super();
-        
-    }
 }
---
+++ NicholasYoonProjectCaravan/src/modules/my/trendingSellers/trendingSellers.js
@@ -0,0 +1,163 @@
+import { LightningElement } from 'lwc';
+
+export default class TrendingSellers extends LightningElement {
+    trendingSellersList = [
+        {
+            sellerName: "Harold Smith Armory Co",
+            sellerImgURL: "./resources/images/emblems/emblem1.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Leather Gloves",
+                    ItemImgURL: "./resources/images/trendingSellerItems/armoredGloves.png",
+                    ItemPrice: "$168"
+                },
+                {
+                    ItemName: "Steel Chestplate",
+                    ItemImgURL: "./resources/images/trendingSellerItems/armoredGloves.png",
+                    ItemPrice: "$960"
+                },
+                {
+                    ItemName: "Steel Toe Boots",
+                    ItemImgURL: "./resources/images/trendingSellerItems/armoredGloves.png",
+                    ItemPrice: "$332"
+                },
+                {
+                    ItemName: "Chainmail",
+                    ItemImgURL: "./resources/images/trendingSellerItems/armoredGloves.png",
+                    ItemPrice: "$525"
+                },
+                {
+                    ItemName: "Shoulder Pads",
+                    ItemImgURL: "./resources/images/trendingSellerItems/armoredGloves.png",
+                    ItemPrice: "$49"
+                }   
+            ]
+        },
+        {
+            sellerName: "Chesire Grove Brewing Inc",
+            sellerImgURL: "./resources/images/emblems/emblem3.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Potion1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Potion2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Potion3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        },
+        {
+            sellerName: "Fleeting Feather Raiding Group",
+            sellerImgURL: "./resources/images/emblems/emblem2.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Item1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        },
+        {
+            sellerName: "Enigma Enchanting Services",
+            sellerImgURL: "./resources/images/emblems/emblem4.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Item1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        },
+        {
+            sellerName: "Elven Grove Hatchery",
+            sellerImgURL: "./resources/images/emblems/emblem5.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Item1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        },
+        {
+            sellerName: "East Blade Raiding Group",
+            sellerImgURL: "./resources/images/emblems/emblem6.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Item1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        },
+        {
+            sellerName: "Unholy Ram Cursing Services",
+            sellerImgURL: "./resources/images/emblems/emblem7.png",
+            sellerTopItems: [
+                {
+                    ItemName: "Item1",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item2",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+                {
+                    ItemName: "Item3",
+                    ItemImgURL: "",
+                    ItemPrice: ""
+                },
+            ]
+        }
+    ]
+}
